56b12ad16efe96b92116c8d1fd8b49d4b2efe3b6
Sumit Makkar <makkarsumit1986@gmail.com>
2023-05-09 22:38:46 +0100

    Precompiled Header File Added

--- premake5.lua
+++ premake5.lua
@@ -18,6 +18,9 @@ project "Shoonya"
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
+    pchheader "shpch.h"
+    pchsource "Shoonya/src/shpch.cpp"
+
     files
     {
         "%{prj.name}/src/**.h",
